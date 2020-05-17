769abe688c3acd645e384a716fa870fed3f02761
Chiva Chen <q932104843@outlook.com>
2020-05-17 11:06:57 +0800

    添加开源协议声明

--- Blog/Mango.Service.Blog/Services/ArticleService.cs
+++ Blog/Mango.Service.Blog/Services/ArticleService.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*--------------------------------------------------------------------------*/
+//
+//  Copyright 2020 Chiva Chen
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+/*--------------------------------------------------------------------------*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,21 +32,42 @@ namespace Mango.Service.Blog.Services
     /// </summary>
     public class ArticleService : IArticleService
     {
+        /// <summary>
+        /// 添加文章
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public Task<ApiResult> AddArticleAsync(AddArticleRequest request, long userId)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 点赞文章
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public Task<ApiResult> ArticleLikeAsync(ArticleLikeRequest request)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 查询文章详情
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public Task<ApiResult<ArticleDetailResponse>> QueryArticleDetailAsync(ArticleDetailRequest request)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 查询文章分页列表
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public Task<ApiResult<PageList<ArticlePageListResponse>>> QueryArticlePageAsync(ArticlePageRequest request)
         {
             throw new NotImplementedException();
