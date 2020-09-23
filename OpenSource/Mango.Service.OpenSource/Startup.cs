5511fd15c4408fd1f73584cd3e78b717aea49f03
Chiva Chen <q932104843@outlook.com>
2020-09-23 22:31:43 +0800

    修改配置文件

--- OpenSource/Mango.Service.OpenSource/Startup.cs
+++ OpenSource/Mango.Service.OpenSource/Startup.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mango.Core.Authentication.Extension;
+using Mango.Core.Converter;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -25,21 +28,67 @@ namespace Mango.Service.OpenSource
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(o =>
+                {
+                    o.JsonSerializerOptions.Converters.Add(new IntConverter());
+                    o.JsonSerializerOptions.Converters.Add(new NullableIntConverter());
+                    o.JsonSerializerOptions.Converters.Add(new LongConverter());
+                    o.JsonSerializerOptions.Converters.Add(new NullableLongConverter());
+                    o.JsonSerializerOptions.Converters.Add(new DateTimeConverter());
+                    o.JsonSerializerOptions.Converters.Add(new NullableDateTimeConverter());
+                });
+
+            #region  ⁄»®≈‰÷√
+
+            var policyKeyPair = new Dictionary<string, string>
+            {
+                {"client","mango.opensource" },
+                {"admin","mango.admin" }
+            };
+
+            services.AddMangoJwtPolicy(policyKeyPair, opt =>
+            {
+                opt.Key = "";
+                opt.Issuer = "hahamango.cn";
+            });
+
+            #endregion
+
+            #region øÁ”Ú≈‰÷√
+
+            services.AddCors(config =>
+            {
+                config.AddPolicy("all", p =>
+                {
+                    p.SetIsOriginAllowed(op => true)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+                });
+            });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors("all");
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
