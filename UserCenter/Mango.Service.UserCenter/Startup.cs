98b9bd57d3ee37c5aa83631f818d75fe9ed7cf3f
Chiva Chen <q932104843@outlook.com>
2020-10-02 11:01:54 +0800

    修改部分配置中心配置

--- UserCenter/Mango.Service.UserCenter/Startup.cs
+++ UserCenter/Mango.Service.UserCenter/Startup.cs
@@ -36,12 +36,17 @@ namespace Mango.Service.UserCenter
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(
+            IConfiguration configuration, 
+            IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -58,15 +63,60 @@ namespace Mango.Service.UserCenter
                 });
 
             #region 配置中心
-            var token = Configuration["Consul:Token"];
-            var configKey = Configuration["Service:ConfigKey"];
-            var consulIp = Configuration["Consul:Ip"];
-            var port = Configuration["Consul:Port"];
+            var moduleConfig = default(MangoServiceConfiguration);
+            var globalConfig = default(MangoServiceConfiguration);
+            if (Env.IsDevelopment())
+            {
+                //测试环境
+                globalConfig = new MangoServiceConfiguration
+                {
+                    JwtKey = @"MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDJQJY/vIlQV8Hm
+y41w6zSJRL7zo/57uj0T6OrJXgzo9pk5mtCWKW/+KTmQ7OCZVaIXKq+5OW1ibkjI
+QDJFRBCbUoUN8799GiadCQJDzpcaGh5ev3wjxzqG8yR0bkkgMNkoYhX7IwJ+6B+1
+zYsyQeVvhztyO2Rj79lId7oSO4Ku2qDomNe1LaBQP6rESKwdx6TIWPUr4CdbnLGX
+AmJ95P66RQ8mqfeHbqvlbZhK/eUF0kNNxbCV0qH4lTFSugSMQaKRNg0H1tlc86dp
+lyZpvf8KEekmNYwY1TZSTC4HwVzDiUn6+yh5EbkGlcC7tO1ClntGHwBGjukLKBk5
+sjx5BgT/AgMBAAECggEBAK7qvOw+sNYswDIZnyCSvYHFR3Y5hhkBwM5KNa65WN0u
+X/TKUxsAfv9X01ncGEYNQKmEB2Ekwaa4lfe+nDLJuulU6qI6xac1EHSSfO50Y65j
+HxxYr8vlAECEmZ28sUASVNwdjF9PiX7Fv7HjKWWQEptB3XAmoNWfhKnQrd/k62uO
+u4+n3Uej/cH+l1RpQQDqy15ojSIBwBDpZFVPJOt/oQpBgF0V/3NxJSiwAnJCWau7
+vGeqFQPlNKW0AldE0Kv3l67FTXC97nnpWdihIvBM0sc8FLNGGg6B4GLTct+Yf5R0
+mPI2KHL3o7TSburKcA03AQ+c2EQ3yh9edgRDa7G4pIECgYEA+yjIWIRU20TGDx/R
+5ox0bdlZyeSQggwuS8rY2k6sU2hFa513WFB5R6duv47QVKEwGKlnccGrtvU2vNQF
+fHYyB9odpkMCQphSUEKy2UrWEB4LJmekUFP4+AjXO0JOEsrQ88hXeSnpC2UXuZe5
+fWn+udeUYrEFondCilBomJzA6r8CgYEAzSGQSXokpAec5Cvm0mTGIM1/H7nG7Jm2
+BbJ5oyyieebGi35GY9RYrCpOIUkP01cBQaRhIv7ftlu6f4lEH/VpxCIRnA7AROsl
+rkdQVAkWU99szd2/6zcAoAs6vU1n5SWF4cF6E/xMCney+x8tGYMWozRwha27uip+
+YqJ4DY1ktcECgYEAmuE1WtCP+39Xm7AFomRuz+a+peZfUejnkvxnKWqWYiL
+Nhy6DWPEub/53JZhsHOW5OGHYJWqoZslnvDMPWdV7VdZJ3QDHpdi7vhlNR8xxQcY
+nqiJ2XqqL1LeDlyfqhWbS456tZornTlhG2OnvzafvJRpYxykHeMj/Sh9FsUCgYAy
+kLJ7mlNL5+CB0lycwmCgl2ddz7K8ggt/jgYz9f27JOsOWbtKQn71OZx20gbHpuvV
+XYrgUIme7y+i3phfdGR1B5zlpjE5C+oG8udXP8I0PKAagy4a8j0CNqJtJZaVwtEk
+3EeWg5vO/MCu7Hl2j3zWEEgoe7IJ6w2qjLghRxhrQQKBgQCiAnVt7AAIokPYBF9X
+wLvUq9MZvk+kbRmRulIKKDSjRAGlxPK3wG416xUfEEox/wmuQicWsb83SOqrSX6Z
+vFixHmPWpvv8a0r26gmdHyhMmXTcl2P5RHdJ/iCBKj9/pIAabhenN/vClnsB8vv/
+tN9fcep4jGpay5xZ0Nj2fSWygw=="
+                };
+                moduleConfig = new MangoServiceConfiguration
+                {
+                    ValidIssuer = "test.cn",
+                    ValidAudience ="mango.usercenter",
+                    DbConnectString = "server=localhost;database=mytest;user=root;password=228887"
+                };
+            }
+            else
+            {
+                //生产环境
+                var token = Configuration["Consul:Token"];
+                var configKey = Configuration["Service:ConfigKey"];
+                var consulIp = Configuration["Consul:Ip"];
+                var consulPort = Configuration["Consul:Port"];
 
-            var config = new MangoConfig($"http://{consulIp}:{port}", token);
+                var config = new MangoConfig($"http://{consulIp}:{consulPort}", token);
 
-            var moduleConfig = config.GetConfig(configKey).Result;
-            var globalConfig = config.GetConfig("mango/global").Result;
+                moduleConfig = config.GetConfig(configKey).Result;
+                globalConfig = config.GetConfig("mango/global").Result;
+            }
             #endregion
 
             #region 跨域配置
@@ -101,7 +151,7 @@ namespace Mango.Service.UserCenter
 
             services.AddMangoJwtPolicy(policyKeyPair, opt =>
             {
-                opt.Key = moduleConfig.JwtKey;
+                opt.Key = globalConfig.JwtKey;
                 opt.Issuer = moduleConfig.ValidIssuer;
             });
 
@@ -130,14 +180,14 @@ namespace Mango.Service.UserCenter
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
+        public void Configure(IApplicationBuilder app, IHostApplicationLifetime lifetime)
         {
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
 
-            if (env.IsDevelopment())
+            if (Env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -153,16 +203,17 @@ namespace Mango.Service.UserCenter
             });
 
             #region 服务注册
-            if (env.IsProduction())
+            if (Env.IsProduction())
             {
+                var token = Configuration["Consul:Token"];
                 var consulIp = Configuration["Consul:Ip"];
-                var port = Configuration["Consul:Port"];
+                var consulPort = Configuration["Consul:Port"];
                 var serviceName = Configuration["Service:Name"];
                 var servicePort = Configuration["Service:Port"];
                 var healthCheck = Configuration["Service:HealthCheck"];
                 var currentIp = NetworkHelper.FirstInternalLocalAddress().ToString();
 
-                var rc = new ConsulRegistration($"http://{consulIp}:{port}")
+                var rc = new ConsulRegistration($"http://{consulIp}:{consulPort}", token)
                 {
                     HealthCheckUrl = $"http://{currentIp}:{servicePort}/{healthCheck}"
                 };
