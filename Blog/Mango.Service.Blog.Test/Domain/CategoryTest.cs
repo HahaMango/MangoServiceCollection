b18c3775a0cc8b2a20e0d2e1732dd54f5a9be399
Chiva Chen <q932104843@outlook.com>
2020-11-22 10:26:34 +0800

    补充文章分类测试

--- Blog/Mango.Service.Blog.Test/Domain/CategoryTest.cs
+++ Blog/Mango.Service.Blog.Test/Domain/CategoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using Mango.Service.Blog.Domain.AggregateModel.CategoryAggreate;
 using Mango.Service.Blog.Domain.AggregateModel.Enum;
+using Mango.Service.Blog.Domain.Event;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -30,6 +31,58 @@ namespace Mango.Service.Blog.Test.Domain
         public void CreateCategoryWithNullNameTest()
         {
             Assert.Throws<ArgumentNullException>(() => new Category(123, null));
+        }
+
+        [Fact]
+        public void DeleteCategoryTest()
+        {
+            var category = new Category(123, "默认分类");
+            category.Delete();
+
+            Assert.NotNull(category);
+            Assert.Equal(EntityStatusEnum.Deleted, category.Status);
+        }
+
+        [Fact]
+        public void EditCategoryTest()
+        {
+            var category = new Category(123, "默认分类");
+            category.Edit("编辑分类");
+
+            Assert.NotNull(category);
+            Assert.Equal("编辑分类", category.CategoryName);
+        }
+
+        [Fact]
+        public void SetDefaultTest()
+        {
+            var category = new Category(123, "默认分类");
+            category.SetDefault();
+
+            Assert.NotNull(category);
+            Assert.Equal(1, category.IsDefault);
+        }
+
+        [Fact]
+        public void SetDefaultPublishDomainEvent()
+        {
+            var category = new Category(123, "默认分类");
+            category.SetDefault();
+
+            var domainEvent = category.DomainEvents[0];
+
+            Assert.NotNull(category);
+            Assert.IsType<SetDefaultCategoryEvent>(domainEvent);
+        }
+
+        [Fact]
+        public void CancelDefaultTest()
+        {
+            var category = new Category(123, "默认分类");
+            category.CancelDefault();
+
+            Assert.NotNull(category);
+            Assert.Equal(0, category.IsDefault);
         }
     }
 }
